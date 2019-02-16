b8b1cb78a76ba1920b07814100d57b025ce2b82e
kayeschong <yukai_chong@mymail.sutd.edu.sg>
2019-02-16 11:10:02 +0800

    Move paddles

--- Pong.py
+++ Pong.py
@@ -1,5 +1,9 @@
 import turtle
 
+def paddle_move(paddle, dy):
+    paddle.sety(paddle.ycor() + dy)
+
+
 wn = turtle.Screen()
 wn.title("Kai's Pong")
 wn.bgcolor("black")
@@ -18,6 +22,14 @@ paddle_a.shapesize(stretch_wid=10, stretch_len=2)
 paddle_a.penup()
 paddle_a.goto(-700, 0)
 
+# Paddle A move
+def paddle_a_up():
+    paddle_move(paddle_a, 40)
+
+def paddle_a_down():
+    paddle_move(paddle_a, -40)
+
+
 # Paddle B
 paddle_b = turtle.Turtle()
 
@@ -28,6 +40,14 @@ paddle_b.shapesize(stretch_wid=10, stretch_len=2)
 paddle_b.penup()
 paddle_b.goto(700, 0)
 
+#Paddle B move
+def paddle_b_up():
+    paddle_move(paddle_b, 40)
+
+def paddle_b_down():
+    paddle_move(paddle_b, -40)
+
+
 #Ball
 ball = turtle.Turtle()
 
@@ -37,9 +57,12 @@ ball.color("white")
 ball.shapesize(stretch_wid=2, stretch_len=2)
 ball.penup()
 
-
-
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 # Main game loop
 while True:
+    
     wn.update()
-    wn.exitonclick()
